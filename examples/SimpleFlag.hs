86c3a29fc31879e714c5e9c34e9965467a419778
Jose Raymundo Cruz <jose.r.cruz01@gmail.com>
2014-04-18 14:57:00 -0400

    adding optionally required documentation

--- examples/SimpleFlag.hs
+++ examples/SimpleFlag.hs
@@ -1,27 +1,14 @@
 import System.Console.HsOptions
 
-userName = make ( "database"
-    , "the user name of the app"
-    , [ parser stringParser
-    , defaultIs "prod.local"
-    , emptyValueIs "prod.local"
-    ]
-    )
+userName = make ( "user_name"
+                , "the user name of the app"
+                , [ parser stringParser
+                  , aliasIs ["u"]
+                  ]
+                )
 userAge = make ("age", "the age of the user", [parser intParser])
 
-log_memory = make ( "log_memory"
-    , "if set to true the memory usage will be logged"
-    , boolFlag)
-
-
-log_output = make ( "log_output"
-    , "where to save the log. required if 'log_memory' is true"
-    , [ maybeParser stringParser
-    , requiredIf (\ flags -> flags `get` log_memory == True)
-    ]
-    )
-
-flagData = combine [flagToData userName, flagToData log_output, flagToData log_memory]
+flagData = combine [flagToData userName, flagToData userAge]
 
 main :: IO ()
 main = processMain "Simple example for HsOptions."
@@ -31,9 +18,11 @@ main = processMain "Simple example for HsOptions."
                    defaultDisplayHelp
 
 success :: ProcessResults -> IO ()
-success (flags, args) = do putStrLn ("Hello " ++ flags `get` userName)
-                           putStrLn ("log_memory " ++ show (flags `get` log_memory))
-                           putStrLn ("log_output " ++ show (flags `get` log_output))
+success (flags, args) = do let nextAge = (flags `get` userAge) + 5
+                           putStrLn ("Hello " ++ flags `get` userName)
+                           putStrLn ("In 5 years you will be " ++
+                                     show nextAge ++
+                                     " years old!")
 
 failure :: [FlagError] -> IO ()
 failure errs = do putStrLn "Some errors occurred:"
